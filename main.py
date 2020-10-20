6d7da9b9774e43afcda48d19c0033df397b2472f
repl.it user <replituser@example.com>
2020-10-20 09:10:43 +0000

    Added code to demonstrate a simple list.

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello world")
+def welcome():
+    print("What type of animal are you: ")
+    animal = input()
+    print("Pride Rock welcomes you {}.".format(animal))
+    print("Come and celebrate with us!")
+
+
+welcome()
+
+def visit(place):
+    if (place == "The Elephant Graveyard"):
+        print("Oh no! There are hyenas here!")
+    else:
+        print("This place is interesting.")
+
+visit("The Elephant Graveyard")
+visit("The Jungle")
+
+def roar(num_roars):
+    for roar in range(num_roars):
+        print("roar!")
+    print("ROAR!!!")
+    
+roar(3)
